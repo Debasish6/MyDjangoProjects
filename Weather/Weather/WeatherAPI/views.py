15512f30c23dab59d0c037448dcbf5d522f18b38
Debasish6 <iamdebasishghosh6@gmail.com>
2024-08-07 23:07:04 +0530

    Final Commit

--- Weather/Weather/WeatherAPI/views.py
+++ Weather/Weather/WeatherAPI/views.py
@@ -14,11 +14,16 @@ def home(request):
         whole_data = json.loads(json_data)
         
         data ={
-            'country': str(whole_data['location']['country']),
+            'country': str(whole_data['location']['name'])+','+str(whole_data['location']['region'])+','+str(whole_data['location']['country']),
             'coordinate': str(whole_data['location']['lat']) + ' ' + str(whole_data['location']['lon']),
-            'temp':str(whole_data["current"]['temp_c'])+ 'C',
-            'pressure':str(whole_data["current"]['pressure_mb']),
-            'humidity':str(whole_data["current"]['humidity']),
+            'temp':str(whole_data["current"]['temp_c'])+ '°C',
+            'pressure':str(whole_data["current"]['pressure_mb'])+' mb',
+            'humidity':str(whole_data["current"]['humidity'])+'%',
+            'wind_speed':str(whole_data['current']['wind_kph'])+' Kph',
+            'feelslike':str(whole_data['current']['feelslike_c'])+'°C',
+            'condition':str(whole_data['current']['condition']['text']),
+            'condition_icon':str(whole_data['current']['condition']['icon']),
+            
         }
         
         print(data)
