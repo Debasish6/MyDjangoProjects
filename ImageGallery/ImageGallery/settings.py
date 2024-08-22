0b349e650879bde6836b5a1fe3774719ac145613
Debasish6 <iamdebasishghosh6@gmail.com>
2024-08-22 09:04:04 +0530

    First Commit Successfully Done

--- ImageGallery/ImageGallery/settings.py
+++ ImageGallery/ImageGallery/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR,'static/img'
+    BASE_DIR / 'images'
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
