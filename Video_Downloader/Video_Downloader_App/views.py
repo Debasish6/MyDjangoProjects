6adb130878d7edd275684051f6b78c8cd1df78c5
Debasish6 <iamdebasishghosh6@gmail.com>
2024-08-25 14:13:19 +0530

    Views updated successfully

--- Video_Downloader/Video_Downloader_App/views.py
+++ Video_Downloader/Video_Downloader_App/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render,HttpResponse
-
+from pytube import *
 
 # Create your views here.
 def home(request):
+   if request.method == 'POST':
+      link = request.POST['link']
+      video = YouTube(link)
+      
+      stream = video.streams.get_lowest_resolution()
+      stream.download()
+      return render(request,'index.html')
    return render(request,'index.html')
