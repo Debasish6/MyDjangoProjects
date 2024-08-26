6d868aa297594209e7053084454809ac1183bb25
Debasish6 <iamdebasishghosh6@gmail.com>
2024-08-26 09:40:37 +0530

    Design Commited Successfully

--- Translator/TranslatorApp/views.py
+++ Translator/TranslatorApp/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from translate import Translator
 
 # Create your views here.
+
 def home(request):
     if request.method == 'POST':
         text = request.POST['translate']
         lang = request.POST['language']
+        model = Translator(to_lang=lang)
+        translated_text = model.translate(text)
+        return render(request,'translate.html',{'context':translated_text})
     return render(request,'translate.html')
