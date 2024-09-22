91b20662a6d26331a1b1ea8bfcb0270520a655a9
Debasish6 <iamdebasishghosh6@gmail.com>
2024-09-22 21:28:26 +0530

    URL Shortener completed

--- URLShortner/URLShortnerApp/views.py
+++ URLShortner/URLShortnerApp/views.py
@@ -6,7 +6,7 @@ def home(request):
     if request.method == 'POST':
         url = request.POST.get('url')
         headers = {
-            'Authorization': 'Bearer 23dc3fe0cd09149fda7d40b7ced6a13aa55d9d05',
+            'Authorization':'Bearer 23dc3fe0cd09149fda7d40b7ced6a13aa55d9d05',
             'Content-Type':'application/json'
         }
         data ={
@@ -14,14 +14,14 @@ def home(request):
             'domain':'bit.ly'
         }
         
-        json_data = json.dumps(data) #It converts data to json data
+        data = json.dumps(data) #It converts data to json data
         
-        response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=json_data)
+        response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, data=data)
         
         short_url=json.loads(response.text)
         print(short_url)
-        # context ={
-        #     'short-url': short_url['link'],
-        # }
-        return render(request,'index.html')
+        context ={
+            'short-url': short_url['link'],
+        }
+        return render(request,'index.html',context)
     return render(request,'index.html')
